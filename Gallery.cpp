666efa1a9592a384a71b5d892bb3c9821d85749c
Toxey <gabybialyk3@gmail.com>
2020-03-14 14:58:15 +0200

    Added data&name feature

--- Gallery.cpp
+++ Gallery.cpp
@@ -1,32 +1,14 @@
 #include <iostream>
 #include <string>
+#include <chrono>
+#include <ctime>
+
 #include "MemoryAccess.h"
 #include "AlbumManager.h"
 
 
-int getCommandNumberFromUser()
-{
-	std::string message("\nPlease enter any command(use number): ");
-	std::string numericStr("0123456789");
-	
-	std::cout << message << std::endl;
-	std::string input;
-	std::getline(std::cin, input);
-	
-	while (std::cin.fail() || std::cin.eof() || input.find_first_not_of(numericStr) != std::string::npos) {
+int getCommandNumberFromUser();
 
-		std::cout << "Please enter a number only!" << std::endl;
-
-		if (input.find_first_not_of(numericStr) == std::string::npos) {
-			std::cin.clear();
-		}
-
-		std::cout << std::endl << message << std::endl;
-		std::getline(std::cin, input);
-	}
-	
-	return std::atoi(input.c_str());
-}
 
 int main(void)
 {
@@ -36,8 +18,11 @@ int main(void)
 	// initialize album manager
 	AlbumManager albumManager(dataAccess);
 
+	// Get currect time.
+	auto timenow = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 
 	std::string albumName;
+	std::cout << "Gabriel Bialyk || " << ctime(&timenow) << std::endl;
 	std::cout << "Welcome to Gallery!" << std::endl;
 	std::cout << "===================" << std::endl;
 	std::cout << "Type " << HELP << " to a list of all supported commands" << std::endl;
@@ -55,3 +40,26 @@ int main(void)
 }
 
 
+int getCommandNumberFromUser()
+{
+	std::string message("\nPlease enter any command(use number): ");
+	std::string numericStr("0123456789");
+
+	std::cout << message << std::endl;
+	std::string input;
+	std::getline(std::cin, input);
+
+	while (std::cin.fail() || std::cin.eof() || input.find_first_not_of(numericStr) != std::string::npos) {
+
+		std::cout << "Please enter a number only!" << std::endl;
+
+		if (input.find_first_not_of(numericStr) == std::string::npos) {
+			std::cin.clear();
+		}
+
+		std::cout << std::endl << message << std::endl;
+		std::getline(std::cin, input);
+	}
+
+	return std::atoi(input.c_str());
+}
